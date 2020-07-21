e482a498aae8a6b25bcc3e97bddceac3bab42fc8
SeoYng <tjdud0123@naver.com>
2020-07-21 15:40:13 +0900

    Create filter.vue

---
+++ filter.vue
@@ -0,0 +1,18 @@
+<!--filter 속성 예제-->
+
+<!--.vue 파일-->
+<span>{{ createdAt | moment('YYYY-MM-DD') }}</span>
+<script>
+export default {
+    data() {
+        return {
+            createdAt:new Date()
+        }
+    },
+    filters: {
+        dayFilter(date) {
+            return moment(date).format('YYYY.MM.DD hh:mm')
+        }
+    }
+}
+</script>
