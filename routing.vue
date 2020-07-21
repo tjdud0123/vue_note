060635ed9e7a0aba71b27af05546874104e2f1b7
SeoYng <tjdud0123@naver.com>
2020-07-21 16:24:16 +0900

    Create routing.vue

---
+++ routing.vue
@@ -0,0 +1,22 @@
+<!--.vue 파일 - 링크 넘기기 전 -->
+<router-link :to="{name : 'componentName', params : { param1 : type, param2 : id }}">
+</router-link>
+
+<!--.vue 파일 - 링크 이동 후 -->
+<a @click="$router.go(-1)">뒤로가기</a>
+
+<script>
+import Router from "vue-router";
+    
+export default{
+    created(){
+        console.log(this.$route.params.param1)
+        console.log(this.$route.params.param2)
+    }
+}
+</script>
+
+<!--routes.js-->
+import ComponentName from '..componentName'
+
+{ path: '/articles/:param1/:param2', name: 'componentName', component: ComponentName }
