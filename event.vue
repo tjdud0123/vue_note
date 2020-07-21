11eeaa3933cf91bc045dd14650deae887337ed3d
SeoYng <tjdud0123@naver.com>
2020-07-21 15:36:59 +0900

    Create event.vue

---
+++ event.vue
@@ -0,0 +1,22 @@
+<!-- 클릭 이벤트 전파가 중단됩니다 -->
+<a v-on:click.stop="doThis"></a>
+
+<!-- 제출 이벤트가 페이지를 다시 로드 하지 않습니다 -->
+<form v-on:submit.prevent="onSubmit"></form>
+
+<!-- 수식어는 체이닝 가능합니다 -->
+<a v-on:click.stop.prevent="doThat"></a>
+
+<!-- 단순히 수식어만 사용할 수 있습니다 -->
+<form v-on:submit.prevent></form>
+
+<!-- 이벤트 리스너를 추가할 때 캡처모드를 사용합니다 -->
+<!-- 즉, 내부 엘리먼트를 대상으로 하는 이벤트가 해당 엘리먼트에서 처리되기 전에 여기서 처리합니다. -->
+<div v-on:click.capture="doThis">...</div>
+
+<!-- event.target이 엘리먼트 자체인 경우에만 트리거를 처리합니다 -->
+<!-- 자식 엘리먼트에서는 안됩니다 -->
+<div v-on:click.self="doThat">...</div>
+
+<!-- only call `vm.submit()` when the `key` is `Enter` -->
+<input v-on:keyup.enter="submit">
