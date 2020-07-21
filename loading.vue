7ca161ded021f0bbc0a23135a0fed4127d8d8eef
SeoYng <tjdud0123@naver.com>
2020-07-21 16:12:48 +0900

    Create loading.vue

---
+++ loading.vue
@@ -0,0 +1,36 @@
+<!--로딩구현-->
+
+<!--.vue 파일-->
+<div class="container" v-if="isLoaded">
+    보이는부분
+</div>
+
+<div v-if="!isLoaded" class="justify-content-center">
+    <div class="text-center" style="padding: 12rem;">
+         <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status" />
+    </div>
+</div>
+
+<script>
+export default {
+    data() {
+        return {
+            data: [],
+            isLoaded: false,
+        }
+    },
+    created() {
+        this.getData()
+    },
+    methods: {
+        getData() {
+            this.isLoaded = false;
+            this.$apiService.getData({})
+                .then(response => {
+                    this.data = response.data.data;
+                    this.isLoaded = true;
+                })
+        }
+    }
+}
+</script>
