360d9f600cce12323a3a9dfc5d4456f5c7297b5c
SeoYng <tjdud0123@naver.com>
2020-07-23 16:00:25 +0900

    swiper 예제

---
+++ swiper.vue
@@ -0,0 +1,49 @@
+<!--https://github.surmon.me/vue-awesome-swiper/-->
+
+<template>
+  <div class="example-3d">
+    <swiper class="swiper" :options="swiperOption">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+  import 'swiper/css/swiper.css'
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide
+    },
+    data() {
+      return {
+        swiperOption: {
+          effect: 'coverflow',
+          grabCursor: true,
+          centeredSlides: true,
+          slidesPerView: 'auto',
+          initialSlide: 0,
+          coverflowEffect: {
+                rotate: 10,
+                stretch: 0,
+                depth: 50,
+                modifier: 3,
+                slideShadows: false
+          },
+          spaceBetween: 0,
+          speed: 800,
+          direction: 'horizontal',
+          slidesPerView: 4,
+          loopedSlides: 4,
+          loop: true
+        }
+      }
+    }
+  }
+</script>
