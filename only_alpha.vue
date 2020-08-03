8f885d3defb51fcc3b93f4feffeb117809fc61a8
SeoYng <tjdud0123@naver.com>
2020-08-03 17:10:05 +0900

    한글입력 방지

---
+++ only_alpha.vue
@@ -0,0 +1,23 @@
+<input
+  type="text"
+  placeholder="영어만 입력해주세요"
+  v-model="name"
+  v-on:input="onlyAlpha($event)"
+/>
+
+<script>
+export default {
+    data(){
+        name:"dafaultName"
+    },
+    method:{
+        onlyAlpha(e) {
+          const regHG = /[ㄱ-ㅎㅏ-ㅣ가-힣]/gi;
+          if (regHG.test(e.data)) {
+            e.target.value = e.target.value.replace(regHG, '');
+            this.formData.name = e.target.value;
+          }
+        }
+    }
+}
+</script>
