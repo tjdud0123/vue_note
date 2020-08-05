c64301b93622e19b7397d6617c17e8a534319dca
SeoYng <tjdud0123@naver.com>
2020-08-05 10:36:13 +0900

    dom접근

---
+++ ref.vue
@@ -0,0 +1,30 @@
+<!--억지로 dom 접근하기-->
+<script>
+ export default{
+    mounted() {
+        const $dropbox = document.querySelectorAll('.selctor');
+        $dropbox.forEach(element => (element.style.minWidth = 0));
+    }
+} 
+</script>
+
+<!-- ref 사용 -->
+<input ref="input">
+<child-component ref="child"></child-component>
+<script>
+import Child from './child.vue';
+    
+export default{
+    components:{
+        Child
+    },
+    methods: {
+      focus: function () {
+        this.$refs.input.focus()
+      }
+    },
+    mounted(){
+      this.$refs.child.childFunc();
+    }
+}
+</script>
