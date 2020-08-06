f55799c4fc327761e80ea81f054e98f69118993a
SeoYng <tjdud0123@naver.com>
2020-08-06 10:13:50 +0900

    window resize에 관한 부분

---
+++ resize.vue
@@ -0,0 +1,16 @@
+<!-- resize 됐을때 css로 해결 불가능한 경우 -->
+<script>
+    export default{
+         methods: {
+            handleResize (event) {
+                console.log(window.innerWidth)
+            }
+        },
+        mounted() {
+            window.addEventListener('resize', this.handleResize)
+        },
+        beforeDestroy() {
+            window.removeEventListener('resize', this.handleResize)
+        }
+    }
+</script>
