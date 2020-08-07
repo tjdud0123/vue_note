46e5a8ac28c090cbd937d4b1fb28cfb0f209f157
SeoYng <tjdud0123@naver.com>
2020-08-07 10:19:56 +0900

    bootstrap with vue

---
+++ bootstrap.vue
@@ -0,0 +1,21 @@
+<!--터미널-->
+npm install vue bootstrap-vue bootstrap
+
+<!--main.js-->
+<script>
+import Vue from 'vue'
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+</script>
+
+<!-- https://www.w3schools.com/bootstrap4/bootstrap_ref_all_classes.asp -->
+<div class="ml-5">Margin Left 5 (~)</div> 
+<div class="ml-sm-5">Margin Left 5 on SM (576~)</div> 
+<div class="ml-md-5">Margin Left 5 on MD (768~)</div> 
+<div class="ml-lg-5">Margin Left 5 on LG (992~)</div>
+<div class="ml-xl-5">Margin Left 5 on XL (1200~)</div> 
+
