68084502f8e65d59b861897253744d0e3c7b81f8
SeoYng <tjdud0123@naver.com>
2020-08-11 10:33:58 +0900

    vue plyr_ modal_ 일시정지 이벤트

---
+++ modal_video.vue
@@ -0,0 +1,29 @@
+<!--비디오 모달 닫기-->
+<base-modal
+  v-bind:show.sync="modals.video"
+  modal-classes="modal-dialog-centered video-body modal-lg play-modal"
+  @close="pauseVideo"
+>
+  <h6 slot="header" class="modal-title">헤더 텍스트</h6>
+  <div>
+    <vue-plyr ref="plyr">
+      <div class="plyr__video-embed">
+        <iframe
+          src="https://"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </vue-plyr>
+  </div>
+</base-modal>
+<!--비디오 멈추기 vue-plyr-->
+
+<!--// 구글링해도 안나오면 콘솔에 쳐보고 함수 찾기-->
+<script>
+    method(){
+        pauseVideo() {
+          //console.log('closed');
+          this.$refs.plyr.player.media.pause();
+        },
+    }
+</script>
