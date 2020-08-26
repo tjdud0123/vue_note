99019171f43be4dcce59974534960ee8f6b9fa3b
SeoYng <tjdud0123@naver.com>
2020-08-26 16:25:11 +0900

    deep selector 추가

--- etc.vue
+++ etc.vue
@@ -1,20 +1,20 @@
 <!--기타 메모-->
 
 <!--html형식으로 넘어오는 텍스트-->
-<div v-html="article.contentHtml"></div>
+<div class="article-content" v-html="article.contentHtml"></div>
 
 <!--컴포넌트 삽입-->
 <script>
-import 컴포넌트이름 from ".vue파일 경로";
+import TutorAvatar from "../../common/tutorAvatar";
 export default {
-  components: {
-    컴포넌트이름
-  }
-};
+    components: {
+        TutorAvatar
+    }
+}
 </script>
 
 <!-- 프롭스 & 이벤트 -->
-<child-component v-on:setIdx="setIdx" :listLength="articles.length" :printLength="printLength"></child-component>
+<pagination v-on:setIdx="setIdx" :listLength="articles.length" :printLength="printLength"></pagination>
 
 props: ['listLength', 'printLength'],
 this.$emit("setIdx", this.order * this.printLength + this.ulIdx * this.printLength * 5);
@@ -43,7 +43,7 @@ data: {
 <!-- v-for - 반복문  -->
 <tbody>
     <tr class="list" v-for="(article, index) in nowList " v-bind:key="index">
-        <td>{{ index }} {{ article }}</td>
+        <td class="text-center">{{ getIndex(index) }}</td>
     </tr>
 </tbody>
     
@@ -56,12 +56,12 @@ data: {
 </div>
 
 <!-- v-model - 데이터 바인딩  -->
-<input type="phone" class="form-control" id="input-phone" placeholder="01012345678" v-model="formData.phoneNumber" />
+<input type="phone" class="form-control" id="input-phone" placeholder="01012345678" v-model="formData.phoneHash" />
     
     data() {
         return {
             formData: {
-                phoneNumber: ""
+                phoneHash: ""
             }
         };
     }
@@ -83,11 +83,117 @@ data: {
 <!-- 라우팅 이전 정보 받아오기  --> 
 <script>
 export default {
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      vm.refCode = from.name;
-      vm.from = from;
-    });
-  }
-};
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            vm.formData.refCode = from.name;
+            vm.from = from;
+        });
+    }
+}
 </script>
+    
+<!--여러개 체크박스 데이터 바인딩-->
+<div id='example-3'>
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+  <label for="jack">Jack</label>
+  <input type="checkbox" id="john" value="John" v-model="checkedNames">
+  <label for="john">John</label>
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+  <label for="mike">Mike</label>
+  <br>
+  <span>체크한 이름: {{ checkedNames }}</span>
+</div>
+<script>
+    export default {
+        data: {
+            checkedNames: []
+          }
+    }
+</script>
+    
+<!--v-model이 관리하는 input을 자동으로 trim 하기 원하면, trim 수정자를 추가하면 됩니다-->
+<input v-model.trim="msg">
+    
+<!-- 사용자 입력이 자동으로 숫자로 형변환 되기를 원하면, v-model이 관리하는 input에 number 수식어를 추가하면 됩니다.-->
+<input v-model.number="age" type="number">
+    
+<!--props를 문자열 배열로 정의하는 대신 유효성 검사 요구사항이 있는 객체를 사용할 수 있습니다.-->
+ props: {
+    // 기본 타입 확인 (`null` 은 어떤 타입이든 가능하다는 뜻입니다)
+    propA: Number,
+    // 여러개의 가능한 타입
+    propB: [String, Number],
+    // 문자열이며 꼭 필요합니다
+    propC: {
+      type: String,
+      required: true
+    },
+    // 숫자이며 기본 값을 가집니다
+    propD: {
+      type: Number,
+      default: 100
+    },
+}
+
+<!-- Inactive components will be cached! -->
+<keep-alive>
+  <component v-bind:is="currentTabComponent"></component>
+</keep-alive>
+
+<!--가끔 JavaScript에서 자식 요소에 직접 접근해야 하는 경우 ref 속성을 이용해 자식 요소에 레퍼런스 ID를 할당하여 해결할 수 있습니다.-->
+<input ref="input">
+methods: {
+  // Used to focus the input from the parent
+  focus: function () {
+    this.$refs.input.focus()
+  }
+}
+    
+<!--애니메이션-->
+  <transition name="bounce">
+    <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </transition>
+    
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+    
+<!-- scss 파일 삽입 -->
+<style lang="scss" scoped>
+@import "./plans.scss";
+</style>
+    
+<!-- style scoped 후 deep selector로 잡아오기 -->
+<style scoped>
+.div1 >>> .div2 {
+  height: calc(72vh - (173px + 1.5rem)) !important;
+}
+</style>
+    
+<!-- 영문, 띄어쓰기, 숫자입력만 - 정규식 이용 -->
+<script>
+const regType1 = /^[A-Za-z\s0-9+]*$/;
+!regType1.test(this.formData.name) alert('영어랑 숫자만 입력해주세요!')
+</script>
+    
+<!--DOM 갱신 후 실행하는 콜백함수-->
+mounted() {
+    this.$nextTick(function() {
+        var dom = document.getElementById('item-0');
+        dom.style.backgroundColor = 'red';
+    });
+
