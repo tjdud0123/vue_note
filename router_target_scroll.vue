309178d8f795900e2549cffc48caa1797d02136d
SeoYng <tjdud0123@naver.com>
2020-08-27 18:22:42 +0900

    Create router_target_scroll.vue
    라우팅 한 뒤 element 타겟팅

---
+++ router_target_scroll.vue
@@ -0,0 +1,13 @@
+<!-- 라우트로 넘아가기 전 페이지 -->
+<router-link v-bind:to="{ path: '/page2', params: { target: true } }"></router-link>
+<!-- 라우트로 넘어간 페이지 -->
+<div id="target" ref="target"></div>
+<script>
+    export default {
+        mounted() {
+            this.$nextTick(function () {
+                this.$route.params?.target && this.$refs.target?.scrollIntoView();
+            });
+        },
+    }
+</script>
